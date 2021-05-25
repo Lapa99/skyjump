e08198062b117c9dca1cc56dccdb307d69036f56
Artem <ph.lapa@yandex.ru>
2021-05-25 18:11:10 +0300

    Initial commit

---
+++ desktop/src/com/skyjump/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skyjump.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skyjump.SkyJump;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SkyJump(), config);
+	}
+}
