a47511cb781dee3912cec86d30d0c18855c786d7
Artem <ph.lapa@yandex.ru>
2021-05-25 18:43:22 +0300

    Itit commit

--- core/src/com/skyjump/SkyJump.java
+++ core/src/com/skyjump/SkyJump.java
@@ -12,7 +12,7 @@ public class SkyJump extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("thebackground.jpg");
 	}
 
 	@Override
